ab6865cf93fabcc610291745134f58cf61d8bf5e
tobiasKaminsky <tobias@kaminsky.me>
2018-10-18 12:01:18 +0200

    add groups
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- src/main/java/com/owncloud/android/ui/activity/UserInfoActivity.java
+++ src/main/java/com/owncloud/android/ui/activity/UserInfoActivity.java
@@ -164,7 +164,7 @@ public class UserInfoActivity extends FileActivity implements Injectable {
         setupToolbar(useBackgroundImage);
         updateActionBarTitleAndHomeButtonByString("");
 
-        adapter = new UserInfoAdapter(null, ThemeUtils.primaryColor(getAccount(), true, this));
+        adapter = new UserInfoAdapter(null);
         mUserInfoList.setAdapter(adapter);
 
         viewModel.getUserInfo().observe(this, this::populateUserInfoUi);
@@ -289,6 +289,8 @@ public class UserInfoActivity extends FileActivity implements Injectable {
             R.string.user_info_website);
         addToListIfNeeded(result, R.drawable.ic_twitter, DisplayUtils.beautifyTwitterHandle(userInfo.getTwitter()),
             R.string.user_info_twitter);
+        addToListIfNeeded(result, R.drawable.ic_group, DisplayUtils.beautifyGroups(userInfo.getGroups()),
+            R.string.user_info_groups);
 
         return result;
     }
